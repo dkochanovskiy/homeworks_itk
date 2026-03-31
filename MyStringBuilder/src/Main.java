c82fc6c950c290d0d40620fae4fa56abf00b472c
Den Kochanovsiy <dkochanovskiy@gmail.com>
2026-03-31 12:31:29 +0300

    add my own realization of StringBuilder

--- MyStringBuilder/src/Main.java
+++ MyStringBuilder/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        MyStringBuilder builder = new MyStringBuilder();
+
+        builder.append("Hello");
+        builder.append(", ");
+        builder.append("world");
+
+        System.out.println("Current value: " + builder);
+
+        builder.deleteLast(5);
+        System.out.println("After deleteLast(5): " + builder);
+
+        builder.undo();
+        System.out.println("After undo(): " + builder);
+
+        builder.append('!');
+        System.out.println("After append('!'): " + builder);
+
+        builder.clear();
+        System.out.println("After clear(): " + builder);
+
+        builder.undo();
+        System.out.println("After undo() again: " + builder);
     }
 }
