29dfaa9f630acd4e4b2a77e2141ab7a617461dc6
Den Kochanovsiy <dkochanovskiy@gmail.com>
2026-04-06 09:57:37 +0300

    stream agregate

---
+++ MyStringBuilder/src/Student.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+class Student {
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+}
